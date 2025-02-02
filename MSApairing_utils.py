0f0bd37600cf583bdbda41f3a2cf2a9a397a73d9
Martin Gordon <martingordon808@gmail.com>
2025-02-01 21:23:15 -0800

    work on MSApairing_utils.py

--- MSApairing_utils.py
+++ MSApairing_utils.py
@@ -16,10 +16,12 @@ import numpy as np
 from collections import defaultdict
 import argparse
 from Bio import AlignIO
+from collections import Counter
 from Bio import SeqIO
 #import pandas as pd
 
-#16384 is current hard limit on the number of MSA records in the A3M AF input
+
+#16384 is current hard limit on the number of MSA records in the A3M AF input; we want to read in the paired sequences, perform our own pairing, then estimate 
 # for now use interleaved as simple to implement and more balanced
 
 # get the HPI mapping 
@@ -29,7 +31,7 @@ from Bio import SeqIO
 
 def getMSAsFromJson(outDir, type='unpaired'):
 
-    jsonPaths = [f for f in glob.glob(outDir+'/*_data.json')]
+    jsonPath = [f for f in glob.glob(outDir+'/*_data.json')]
     
     if len(jsonPath) == 0 or len(jsonPath) > 1:
         raise RuntimeError(f'Expected 1 json file in {output_dir} but found {len(jsonPath)}:\n{jsonPath}')
@@ -50,9 +52,8 @@ def getMSAsFromJson(outDir, type='unpaired'):
             pairedMSAout.write(bm['pairedMsa'])
 
 # return dict of a3m and the output file
-def readA3mMSA(path):
+def readA3M(path):
   allSeqs = collections.defaultdict(str)
-  #namesInOrder = []
 
   with open(path) as fp:
     currentName = ""
@@ -63,12 +64,9 @@ def readA3mMSA(path):
       # start processing
       if line.startswith(">"):
         currentName = line
-        #namesInOrder.append(currentName)
       else:
         assert currentName != "", "Unexpected format, empty sequence name or sequence data before first >"
         allSeqs[currentName] = allSeqs[currentName] + line
-  # return as tuple structure (not sure if the MSA has redundancy so safer maybe...)
-  #return(allSeqs, namesInOrder)
   return(allSeqs)
 
 # get the total number of sequences, look for each sequence to see if it passes or fails the seq filter
@@ -76,7 +74,7 @@ def readA3mMSA(path):
 def filterGappedMSA(msaDict, maxSeqGap=0.9):
 
   print(f"Starting number of records in msa: {len(msaDict)}")
-  print(f"Filtering out msa records with >= {maxSeqGap}  gapped characters in sequence...")
+  print(f"Filtering out msa records with >= {maxSeqGap} % gapped characters...")
 
   filteredDict = collections.defaultdict(str)
   for k,v in msaDict.items():
@@ -86,6 +84,77 @@ def filterGappedMSA(msaDict, maxSeqGap=0.9):
   print(f"Filtered out {len(msaDict) - len(filteredDict)} records in msa")
   print(f"Remainging records: {len(filteredDict)} ")
   return(filteredDict) 
+
+
+# dictionary of species ID and N records assigned to species {speciesID: N sequences}
+# sanity check and counts look good
+def makeSpeciesDictionary(a3m):
+   
+   orgIDs = collections.defaultdict()
+   for k,v in a3m.items():
+      # regex for OX= in the seq header and take taxon ID
+      orgID = re.search('OX[=][0-9]+', k)
+      if orgID:
+        org = re.sub("OX=", "", orgID.group())
+        if org not in orgIDs:
+          # initialise key and count
+          orgIDs[(org)] = 1
+        else:
+          orgIDs[(org)] += 1
+   return(orgIDs)
+
+
+# deduplicate MSA within species level.. dont want replicate species to artificially inflate..
+def deduplicateMSA(msaDict):
+   
+    cleanMSA = collections.defaultdict(str)
+    # track dups within species; so we create a set to capture unique sequences in each
+    speciesSet = collections.defaultdict(set)
+ 
+    for k,v in msaDict.items():
+      orgID = re.search('OX[=][0-9]+', k)
+      if orgID: #get species idx
+        org = re.sub("OX=", "", orgID.group())
+        
+        if v not in speciesSet[org]:
+          cleanMSA[k] = v
+          speciesSet[org].add(v)
+
+    print(f"{len(cleanMSA)} sequences remaining after deduplication")                  
+    return cleanMSA
+
+# get the number of paired records in the paired MSA, to estimate the number of remaining records
+# how does AF performing the species pairing? one to many or 1-1? For safety and first test, t
+# take as the min size of either set, then use remaing space (from ~16k) to populate the MSA
+def getPairedMSADepth(a3m1_path, a3m2_path):
+   
+   a3m1 = readA3M(a3m1_path)
+   a3m2 = readA3M(a3m2_path)
+   
+   print(f"Removing duplicate MSA records at the species level...")
+   clean1 = deduplicateMSA(a3m1)
+   clean2 = deduplicateMSA(a3m2)
+
+   sp1Dict = makeSpeciesDictionary(clean1)
+   sp2Dict = makeSpeciesDictionary(clean2)
+
+   # converting dicitonary to set keeps names only, and set has intersection function to find overlap
+   shared_species = set(sp1Dict).intersection(set(sp2Dict))
+   print(f"found {len(shared_species)} shared species...")
+   
+   #populate subdicts
+   print(f"Subsetting MSAs to shared species")
+   sp1sub = {}; sp2sub = {}
+   for k in shared_species:
+      sp1sub[k] = sp1Dict[k]
+      sp2sub[k] = sp2Dict[k]
+
+   if sum(sp1sub.values()) < sum(sp2sub.values()):
+      print(f'{os.path.basename(a3m1_path)} has the shallowest MSA after deduplication and species pairing:\n{sum(sp1sub.values())}')
+      return(sum(sp1sub.values()))
+   else:
+      print(f'{os.path.basename(a3m2_path)} has the shallowest MSA after deduplication and species pairing:\n{sum(sp2sub.values())}')
+      return(sum(sp2sub.values()))
 
 
 # padding added to A3M MSA rows; I would prefer to add the padding before and after to make the padding clear, but how
